dca6f927f45e0c94a25318a0627551335fa86c50
NoahGinger317 <depnertheginger@gmail.com>
2017-03-03 14:28:58 -0700

    thtrhsyt
    erw

--- team1.py
+++ team1.py
@@ -1,4 +1,5 @@
 ####
+import random
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
@@ -6,17 +7,20 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Change to Literally Anything' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team_Soo_Woo' # Only 10 chars displayed.
+strategy_name = 'betray 3 out of 4 times'
+strategy_description = 'you can dictate the opponents move'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
+    ''' 
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,8 +30,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    if 'b' in their_history[-10:]: # If the other player has betrayed within last 10 rounds, 
+        return 'b'               # Betray.
+    else:
+        if random.random()<0.9: # 90% of the other rounds
+            return 'b'         # Betray
+        else:
+            return 'c' 
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
@@ -40,7 +49,7 @@ def test_move(my_history, their_history, my_score, their_score, result):
     else:
         print("move(" +
             ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
+                       (my_score), str(their_score)])+
             ") returned " + "'" + real_result + "'" +
             " and should have returned '" + result + "'")
         return False
@@ -48,8 +57,8 @@ def test_move(my_history, their_history, my_score, their_score, result):
 if __name__ == '__main__':
      
     # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
+    if test_move(my_history='b',
+              their_history='b', 
               my_score=0,
               their_score=0,
               result='b'):
